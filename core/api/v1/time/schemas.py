44f8077d3fc6fbbf08d6f3650be2dfc5c6712ee0
uzineck <illyakrykalov@gmail.com>
2024-12-11 21:17:38 +0200

    time schema refactoring

--- core/api/v1/time/schemas.py
+++ core/api/v1/time/schemas.py
@@ -4,14 +4,14 @@ from core.apps.common.time.entity import TimeInfo as TimeInfoEntity
 
 
 class TimeInfoOutSchema(Schema):
-    current_week_is_even: bool
-    current_day: int
-    current_lesson: int
+    is_even: bool
+    day: int
+    lesson: int
 
     @classmethod
     def from_entity(cls, entity: TimeInfoEntity) -> 'TimeInfoOutSchema':
         return cls(
-            current_week_is_even=entity.current_week_is_even,
-            current_day=entity.current_day,
-            current_lesson=entity.current_lesson,
+            is_even=entity.current_week_is_even,
+            day=entity.current_day,
+            lesson=entity.current_lesson,
         )
