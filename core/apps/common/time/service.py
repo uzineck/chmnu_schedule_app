3bc847f453b666d3d5bb1f26fd60aa7482160b20
uzineck <illyakrykalov@gmail.com>
2024-12-16 09:53:59 +0200

    time service refactoring

--- core/apps/common/time/service.py
+++ core/apps/common/time/service.py
@@ -3,15 +3,9 @@ from abc import (
     ABC,
     abstractmethod,
 )
-from datetime import datetime
+from datetime import datetime, time
 
-from core.apps.common.time.constants import (
-    END_OF_ACADEMIC_DAY,
-    LESSON_TIMETABLE,
-    START_OF_ACADEMIC_DAY,
-    START_OF_ACADEMIC_SEMESTER_LINE,
-)
-
+from core.apps.common.models import OrdinaryNumber
 
 class BaseTimeService(ABC):
     @abstractmethod
@@ -19,7 +13,12 @@ class BaseTimeService(ABC):
         ...
 
     @abstractmethod
-    def get_current_week_type(self, datetime_now: datetime, start_of_academic_semester: datetime) -> bool:
+    def get_current_week_type(
+            self,
+            datetime_now: datetime,
+            start_of_academic_semester: datetime,
+            start_of_academic_semester_line: bool,
+    ) -> bool:
         ...
 
     @abstractmethod
@@ -27,7 +26,13 @@ class BaseTimeService(ABC):
         ...
 
     @abstractmethod
-    def get_current_lesson(self, datetime_now: datetime) -> int:
+    def get_current_lesson(
+            self,
+            datetime_now: datetime,
+            lesson_timetable: dict[OrdinaryNumber, tuple[time, time]],
+            start_of_academic_day: time,
+            end_of_academic_day: time,
+    ) -> int:
         ...
 
 
@@ -36,13 +41,16 @@ class DatetimeTimeService(BaseTimeService):
         current_time = datetime.now(tz=pytz.timezone('Europe/Kyiv'))
         return current_time
 
-    def get_current_week_type(self, datetime_now: datetime, start_of_academic_semester: datetime) -> bool:
-        # At the start of academic semester the line is above(True) or below(False)
-        start_week_line = START_OF_ACADEMIC_SEMESTER_LINE
+    def get_current_week_type(
+            self,
+            datetime_now: datetime,
+            start_of_academic_semester: datetime,
+            start_of_academic_semester_line: bool,  # At the start of academic semester the line is above(True) or below(False)
+    ) -> bool:
         weeks_since_start = (datetime_now.date() - start_of_academic_semester.date()).days // 7
         # Every even week will be True, every odd week will be False
         # The first week is when weeks_since_start = 0
-        if (weeks_since_start % 2 == 0) == start_week_line:
+        if (weeks_since_start % 2 == 0) == start_of_academic_semester_line:
             return True  # Returns True if the week is above the line
         else:
             return False  # Returns False if the week is below the line
@@ -51,16 +59,22 @@ class DatetimeTimeService(BaseTimeService):
         current_day = datetime_now.isoweekday()
         return current_day
 
-    def get_current_lesson(self, datetime_now: datetime) -> int:
+    def get_current_lesson(
+            self,
+            datetime_now: datetime,
+            lesson_timetable: dict[OrdinaryNumber, tuple[time, time]],
+            start_of_academic_day: time,
+            end_of_academic_day: time,
+    ) -> int:
         current_time = datetime_now.time()
 
         # Lesson time
-        for lesson_number, (start_time, end_time) in LESSON_TIMETABLE.items():
+        for lesson_number, (start_time, end_time) in lesson_timetable.items():
             if start_time <= current_time <= end_time:
                 return lesson_number  # Can be pydantic error(change to lesson_number.value)
 
         # Break time
-        if START_OF_ACADEMIC_DAY <= current_time <= END_OF_ACADEMIC_DAY:
+        if start_of_academic_day <= current_time <= end_of_academic_day:
             return 0
 
         # Out of working time
--- core/apps/common/time/use_case.py
+++ core/apps/common/time/use_case.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
 from typing import Any
 
-from core.apps.common.time.constants import START_OF_ACADEMIC_SEMESTER
+from core.apps.common.time.constants import START_OF_ACADEMIC_SEMESTER, START_OF_ACADEMIC_SEMESTER_LINE, \
+    LESSON_TIMETABLE, START_OF_ACADEMIC_DAY, END_OF_ACADEMIC_DAY
 from core.apps.common.time.entity import TimeInfo as TimeInfoEntity
 from core.apps.common.time.service import BaseTimeService
 
@@ -15,7 +16,13 @@ class GetCurrentTimeInfo:
         current_week = self.time_service.get_current_week_type(
             datetime_now=current_time,
             start_of_academic_semester=START_OF_ACADEMIC_SEMESTER,
+            start_of_academic_semester_line=START_OF_ACADEMIC_SEMESTER_LINE,
         )
         current_day = self.time_service.get_current_day(datetime_now=current_time)
-        current_lesson = self.time_service.get_current_lesson(datetime_now=current_time)
+        current_lesson = self.time_service.get_current_lesson(
+            datetime_now=current_time,
+            lesson_timetable=LESSON_TIMETABLE,
+            start_of_academic_day=START_OF_ACADEMIC_DAY,
+            end_of_academic_day=END_OF_ACADEMIC_DAY,
+        )
         return TimeInfoEntity(current_week_is_even=current_week, current_day=current_day, current_lesson=current_lesson)
